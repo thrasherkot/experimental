b2ba909842e4f2c204faa404c1ddfb47021a4452
thrasherkot <thrasherkot@gmail.com>
2024-02-20 01:02:51 +0300

    update for path

--- src/repo_updater.py
+++ src/repo_updater.py
@@ -1,24 +1,29 @@
 import datetime
+import os
 from git import Repo
 
+
+repo_dir = os.path.dirname(os.path.abspath(__file__)) + '/../'
+os.chdir(repo_dir)
+readme_path = "Readme.md"
 random_string = 'Record for ' + str(datetime.datetime.now())
 
 # Read existing content from Readme.md
-md_file = '../Readme.md'
-with open(md_file, 'r') as file:
+
+with open(readme_path, 'r') as file:
     readme_content = file.read()
 
 # Append the random string to the Readme content
 new_readme_content = readme_content + f"\n{random_string}"
 
 # Write the updated content back to Readme.md
-with open(md_file, 'w') as file:
+with open(readme_path, 'w') as file:
     file.write(new_readme_content)
 
 # Commit changes
-repo = Repo('../')
+repo = Repo(repo_dir)
 index = repo.index
-index.add(['Readme.md'])
+index.add([readme_path])
 index.commit(f"Add new string: {random_string}")
 
 # Push changes to remote repository
